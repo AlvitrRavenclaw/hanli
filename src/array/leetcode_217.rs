8d05bba6b75836ff47c0048ebc66139302d64684
AlvitrRavenclaw <943459049@qq.com>
2020-08-10 20:05:03 +0800

    leetcode 136

--- src/array/leetcode_217.rs
+++ src/array/leetcode_217.rs
@@ -6,12 +6,13 @@ struct Solution;
 impl Solution {
     #[allow(dead_code)]
     pub fn contains_duplicate(nums: Vec<i32>) -> bool {
-        let mut set: HashSet = HashSet::new<i32>();
+        let mut set = HashSet::new();
         for i in 0..nums.len() {
-            if set.contains(nums[i]) {
-                return true
+            if set.contains(&nums[i]) {
+                return true;
+            } else {
+                set.insert(nums[i]);
             }
-            set.insert(nums[i])
         }
         return false
     }
@@ -19,13 +20,13 @@ impl Solution {
 
 #[test]
 fn test_contains_duplicate() {
-    let mut case0 = vec![1, 2, 3, 1];
+    let case0 = vec![1, 2, 3, 1];
     assert_eq!(Solution::contains_duplicate(case0), true);
 
-    let mut case1 = vec![1, 2, 3, 4];
+    let case1 = vec![1, 2, 3, 4];
     assert_eq!(Solution::contains_duplicate(case1), false);
 
-    let mut case2 = vec![1, 1, 1, 3, 3, 4, 3, 2, 4, 2];
+    let case2 = vec![1, 1, 1, 3, 3, 4, 3, 2, 4, 2];
     assert_eq!(Solution::contains_duplicate(case2), true);
 }
 
